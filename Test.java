74564072cd398a03155fdabfb2de07855ee298aa
pqhuy996 <phanquochuy99206@gmail.com>
2024-10-15 08:39:02 +0700

    baitaptrenlop

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        LibaryItem libaryItem = new LibaryItem("VKU", 2024);
+        System.out.println(libaryItem.toString());
+        LibaryItem.Book book = new LibaryItem.Book("dalsjk", 2024, "PQH", 1234);
+        System.out.println(book.getDetails());
+        LibaryItem.Magazine magazine = new LibaryItem.Magazine("dasdasd", 2024, 3214654, "University");
+        System.out.println(magazine.getDetails());
+    }
+}
